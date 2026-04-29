0e08890cd9d18aa7d55cc924583776aab926d1b2
Aman Kumar <aman.it360@gmail.com>
2026-04-29 16:47:17 +0530

    feat: add CSS module declaration and integrate Google Ads script in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5440423066947557"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${inter.variable} ${sora.variable} antialiased max-w-screen`}
       >
